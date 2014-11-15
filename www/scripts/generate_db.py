fa197dfd28f8963f76d7430a73a6efacd11714ae
Nicolas DUBIEN <github@dubien.org>
2014-11-15 01:01:56 +0100

    [new][script] Table creation

---
+++ www/scripts/generate_db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+# This script has to generate the sqlite database
+#
+# Requirements (import from):
+#   -  sqlite3
+#
+# Syntax:
+#   ./generate_db.py
+
+import sqlite3
+
+import sys
+from os import path
+SCRIPT_PATH = path.dirname(__file__)
+DEFAULT_DB = path.join(SCRIPT_PATH, "../mymoney.db")
+
+def generate_tables(db=DEFAULT_DB):
+    conn = sqlite3.connect(db)
+    
+    with conn:
+        c = conn.cursor()
+        
+        # Drop tables if they exist
+        c.execute('''DROP TABLE IF EXISTS node''')
+        c.execute('''DROP TABLE IF EXISTS node_hierarchy''')
+        c.execute('''DROP TABLE IF EXISTS expense''')
+        
+        # Create tables
+        c.execute('''CREATE TABLE IF NOT EXISTS node (
+                        id INTEGER PRIMARY KEY,
+                        title TEXT NOT NULL)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS node_hierarchy (
+                        id INTEGER PRIMARY KEY,
+                        parent_id INTEGER,
+                        child_id INTEGER,
+                        FOREIGN KEY(parent_id) REFERENCES node(id),
+                        FOREIGN KEY(child_id) REFERENCES node(id),
+                        UNIQUE(child_id))''')
+        c.execute('''CREATE TABLE IF NOT EXISTS expense (
+                        id INTEGER PRIMARY KEY,
+                        node_id INTEGER,
+                        title TEXT NOT NULL,
+                        date INTEGER NOT NULL,
+                        value REAL NOT NULL,
+                        FOREIGN KEY(node_id) REFERENCES node(id))''')
+        
+        # Commit the changes
+        conn.commit()
+
+if __name__ == '__main__':
+    generate_tables(DEFAULT_DB)
