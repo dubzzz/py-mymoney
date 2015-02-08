0a9003aa9e3700b726e5e9eb468da6466c20c02b
Nicolas DUBIEN <github@dubien.org>
2015-02-08 21:17:17 +0100

    [expenses] Autocomplete for categories

--- www/static/js/add_expenses_table.js
+++ www/static/js/add_expenses_table.js
@@ -1,5 +1,6 @@
 var XML_EXPENSE_ADD_URL = null;
 var ADD_EXPENSES_TABLE = null;
+var XML_TREES_ELTS = new Array();
 var LAST_EXPENSE_ID = 0;
 
 function readExpenseDate(expense) {
@@ -105,6 +106,171 @@ function reactOnPriceFocus() {
 	}
 }
 
+Array.prototype.sortOnBestScore = function() {
+	this.sort(function(a, b){
+		if(a['score'] < b['score']){
+			return -1;
+		} else if(a['score'] > b['score']) {
+			return 1;
+		} else if (a['rawdisplay'] < b['rawdisplay']) {
+			return -1;
+		} else if (a['rawdisplay'] > b['rawdisplay']) {
+			return 1;
+		}
+		return 0;
+	});
+}
+
+function computeAutocompletePriority(elt, query) {
+	var elt_text = elt['rawdisplay'];
+	var elt_text_lower = elt_text.toLowerCase();
+	var query_lower = query.toLowerCase();
+	
+	var best_origin = -1;
+	var best_score = -1;
+	for (var i=0 ; i!=elt_text_lower.length ; i++) { // Look for a string starting from this element
+		if (query_lower.length == 0 || elt_text_lower[i] != query_lower[0]) {
+			continue;
+		}
+		var padding_pos = 0;
+		var query_pos = 0;
+		for (query_pos = 0 ; i+padding_pos != elt_text_lower.length && query_pos != query_lower.length ; padding_pos++) {
+			if (elt_text_lower[i+padding_pos] == query_lower[query_pos]) {
+				query_pos++;
+			}
+		}
+
+		// Is there a match?
+		// If so, is it better than current one?
+		if (query_pos == query_lower.length) {//match
+			if (best_score == -1 || best_score > padding_pos-query_pos) {
+				best_origin = i;
+				best_score = padding_pos-query_pos;
+				if (best_score == 0) {
+					break;
+				}
+			}
+		}
+	}
+
+	// Highlight match characteristics
+	if (best_score != -1) {
+		var new_elt = elt;
+		new_elt["score"] = best_score;
+		if (query.length == 0) {
+			new_elt['display'] = elt_text;
+		} else {
+			new_elt['display'] = "";
+			var i = 0;
+			var query_pos = 0;
+			for ( ; i != elt_text_lower.length ; i++) {
+				if (i >= best_origin && query_pos != query_lower.length && elt_text_lower[i] == query_lower[query_pos]) {
+					new_elt['display'] += "<b>" + elt_text[i] + "</b>";
+					query_pos++;
+				}
+				else
+				{
+					new_elt['display'] += elt_text[i];
+				}
+			}
+		}
+		return new_elt;
+	}
+	return undefined;
+}
+
+function computeAutocompleteResults(available_elts, query) {
+	var elts_to_display = new Array();
+	for (var i=0 ; i!=available_elts.length ; i++) {
+		var new_elt = computeAutocompletePriority(available_elts[i], query);
+		if (new_elt) {
+			elts_to_display.push(new_elt);
+		}
+	}
+	elts_to_display.sortOnBestScore();
+	return elts_to_display;
+}
+
+function reactOnCategoryKeyUp(event) {
+	// Refresh the content of the autocomplete list
+	
+	// Get autocomplete list or create it if not displayed
+	var categories_td = $(this).parent();
+	var autocomplete_list = categories_td.find(".autocomplete-list");
+	if (autocomplete_list.length == 0) {
+		categories_td.css('position', 'relative');
+		autocomplete_list = $("<ul/>");
+		autocomplete_list.addClass("autocomplete-list");
+		categories_td.append(autocomplete_list);
+	}
+	var position_left = $(this).position()['left'];
+	var position_top = $(this).position()['top'] + $(this).height();
+	autocomplete_list.css('left', position_left + 'px');
+	autocomplete_list.css('top', position_top + 'px');
+	
+	// Get already selected elements position
+	var selected_elt = autocomplete_list.find('.autocomplete-list-selected').first();
+	var selected_elt_id = -1;
+	if (selected_elt.length == 1) {
+		selected_elt_id = parseInt(selected_elt.attr('data-autocomplete-id'));
+	}
+	if (selected_elt_id != -1 && event.keyCode == 13) { // Enter
+		//TODO
+		event.preventDefault();
+		return;
+	}
+	else if (event.keyCode == 38 || event.keyCode == 40) { // Up or Down
+		var autocomplete_elts = autocomplete_list.children();
+		var current_index = 0;
+		for (current_index=0 ; current_index != autocomplete_elts.length ; current_index++) {
+			if ($(autocomplete_elts[current_index]).hasClass('autocomplete-list-selected')) {
+				break;
+			}
+		}
+		if (autocomplete_elts.length > 0) {
+			if (current_index == autocomplete_elts.length) {
+				$(autocomplete_elts[0]).addClass('autocomplete-list-selected');
+			} else if (event.keyCode == 38) {
+				if (current_index > 0) {
+					$(autocomplete_elts[current_index]).removeClass('autocomplete-list-selected');
+					$(autocomplete_elts[current_index -1]).addClass('autocomplete-list-selected');
+				}
+			} else if (event.keyCode == 40) {
+				if (current_index < autocomplete_elts.length -1) {
+					$(autocomplete_elts[current_index]).removeClass('autocomplete-list-selected');
+					$(autocomplete_elts[current_index +1]).addClass('autocomplete-list-selected');
+				}
+			}
+			event.preventDefault();
+			return;
+		}
+	} else if (event.keyCode == 27) {
+		autocomplete_list.remove();
+	}
+	
+	// Create autocomplete list
+	var elts_to_display = computeAutocompleteResults(XML_TREES_ELTS, $(this).val());
+	
+	// Display elements
+	autocomplete_list.empty();
+	for (var i=0 ; i != elts_to_display.length ; i++) {
+		var autocomplete_elt = $("<li/>");
+		autocomplete_elt.attr('data-autocomplete-id', elts_to_display[i]['id']);
+		if (elts_to_display[i]['id'] == selected_elt_id) {
+			autocomplete_elt.addClass('autocomplete-list-selected');
+		}
+		autocomplete_elt.html(elts_to_display[i]['display']);
+		autocomplete_list.append(autocomplete_elt);
+	}
+	if (elts_to_display.length == 0) {
+		autocomplete_list.remove();
+	}
+}
+
+function reactOnCategoryFocusOut() {
+	$(this).parent().find(".autocomplete-list").remove();
+}
+
 function appendExpense() {
 	// Append an empty expense in the form
 	
@@ -150,13 +316,20 @@ function appendExpense() {
 	price_input.attr("type", "text");
 	price_input.attr("pattern", "[-+]?(\\d{1,3}([\\s,]\\d{3})*|\\d+)(\\.\\d{0,2})?"); // 1000.00, 1 000.00, 1000 are valids
 	price_input.attr("placeholder", "0.00");
-	price_input.keyup(reactOnExpenseChange);
 	price_input.focus(reactOnPriceFocus);
 	price_input.focusout(reactOnPriceFocusOut);
+	price_input.keyup(reactOnExpenseChange);
 	price_td.append(price_input);
 	expense.append(price_td);
 
 	var categories_td = $("<td/>");
+	var categories_input = $("<input/>");
+	categories_input.addClass("categories-input");
+	categories_input.attr("type", "text");
+	categories_input.attr("placeholder", "Classify expense");
+	categories_input.focusout(reactOnCategoryFocusOut);
+	categories_input.keyup(reactOnCategoryKeyUp);
+	categories_td.append(categories_input);
 	expense.append(categories_td);
 
 	var actions_td = $("<td/>");
@@ -182,7 +355,32 @@ function appendExpense() {
 	ADD_EXPENSES_TABLE.append(expense);
 }
 
-function initAddExpensesTable(addExpenseUrl) {
+function getTreeNodePath(node) {
+	if (node && node.attr("title")) {
+		var parent_path = getTreeNodePath(node.parent());
+		if (parent_path.length > 0) {
+			return parent_path + " > " + node.attr("title");
+		} else {
+			return node.attr("title");
+		}
+	}
+	return "";
+}
+
+function loadTrees(nodes) {
+	for (var i = 0 ; i != nodes.length ; i++) {
+		var node = $(nodes[i]);
+		var children = node.children();
+		if (children.length > 0) {
+			loadTrees(children);
+		} else {
+			var elt = {rawdisplay: getTreeNodePath(node), id: node.attr("id")};
+			XML_TREES_ELTS.push(elt);
+		}
+	}
+}
+
+function initAddExpensesTable(addExpenseUrl, xmlTreeUrl) {
 	// Initialize expenses table
 	
 	XML_EXPENSE_ADD_URL = addExpenseUrl;	
@@ -190,6 +388,18 @@ function initAddExpensesTable(addExpenseUrl) {
 	ADD_EXPENSES_TABLE.html("");
 	appendExpense();
 	$("button#submit-expenses").click(ajaxSaveAllExpenses);
+
+	$.ajax({
+		type: "get",
+		url: xmlTreeUrl,
+		dataType: "xml",
+		success: function(xml)
+		{
+			XML_TREES_ELTS = new Array();
+			var root_nodes = $(xml).find("trees").first().find("> node");
+			loadTrees(root_nodes);
+		}
+	});
 }
 
 /**
